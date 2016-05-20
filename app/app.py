3b1bcde638e2709e59fdd37e58f37345f96c5c1f
Mateusz Kowalczyk <mateusz.kowalczyk@universalunit.net>
2016-05-20 10:59:46 +0100

    deleted unused login and out

--- app/app.py
+++ app/app.py
@@ -464,28 +464,6 @@ def del_all_answers():
     return redirect(url_for('show_entries'))
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'ppl':
-            error = 'Invalid username'
-        elif request.form['password'] != 'ppl':
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
-
-
 if __name__ == '__main__':
     app.run()
 
